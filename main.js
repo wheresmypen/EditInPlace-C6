d3d14954198d017b7f0c0e8ebc1388b0026a8c02
Daniel O'Brien <wheresmypen@yahoo.com>
2013-10-06 16:05:07 -0600

    for Joanna

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
 $(document).ready(function(){
 	
+var grabField="";
+var stayField="";
 
-var selectToReturn = function(){
-	$(".holder").on("click",appear)
+var selectToSelect = function(){
+	$(".holder").on("click",appear);
 };
 
-var appear = function(){
-	a = $(this).attr('class');
-	index = a.slice(6,7);
-	console.log(index);
+var appear = function(e){
+	event.preventDefault;
+	grabField = $(this).attr('class');
+	index = grabField.slice(6,7);
+	var toob = $("#label"+index).text();
+	$("#label"+index).hide();
+	$("#hLabel"+index).show();
+	$("#vLabel"+index).show();
+	stayField=index;
 };
 
+var selectToRevise = function(e){
+	$(".input").on("blur",renderNew);
+	return false;
+}
 
 
-// $("#hWhole").hide();
+var renderNew = function(e){
+	event.preventDefault;
+	var newOriginal1 = $("#hLabel"+stayField).val();
+	console.log(newOriginal1);
+	$("#hLabel"+stayField).hide();
+	$("#label"+stayField).text($("#hLabel"+stayField).val());
+	$("#label"+stayField).show();
+}
 
-// $(" #hNameLabel, #hMotto, #hLived, .hBlog, #hContext" ).hide();
+
 
 $(".input").hide();
 
 
 
-selectToReturn();
-
+selectToSelect();
+selectToRevise();
 
 
 
